dfc8774292f96199dbc12ce1674a9b1875105ffd
Caio Martins <caio_martins02@yahoo.com.br>
2022-02-23 19:31:37 -0300

    Estudy about Camera manipulating  with expo

--- App.js
+++ App.js
@@ -1,11 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Modal,
+  Image,
+} from "react-native";
+import { Camera } from "expo-camera";
+import { MaterialIcons, FontAwesome, Ionicons } from "@expo/vector-icons";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const [focus, setFocus] = useState(Camera.Constants.AutoFocus.off);
+
+  const camRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const takePicture = async () => {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri);
+      setOpen(true);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.camera}
+        type={type}
+        ref={camRef}
+        flashMode={flash}
+        autoFocus={focus}
+      >
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <MaterialIcons name="flip-camera-ios" size={34} color="#fff" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setFlash(
+                flash === Camera.Constants.FlashMode.off
+                  ? Camera.Constants.FlashMode.on
+                  : Camera.Constants.FlashMode.off
+              );
+            }}
+          >
+            <Ionicons
+              name={flash ? "flash" : "flash-off"}
+              size={24}
+              color="white"
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setFocus(
+                focus === Camera.Constants.AutoFocus.off
+                  ? Camera.Constants.AutoFocus.on
+                  : Camera.Constants.AutoFocus.off
+              );
+            }}
+          >
+            <MaterialIcons
+              name={focus ? "center-focus-strong" : "center-focus-weak"}
+              size={24}
+              color="white"
+            />
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      <TouchableOpacity style={styles.picture} onPress={takePicture}>
+        <FontAwesome name="camera" size={24} color="white" />
+      </TouchableOpacity>
+
+      {capturedPhoto && (
+        <Modal animationType="slide" a transparent={false} visible={open}>
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <TouchableOpacity
+              style={{ margin: 10 }}
+              onPress={() => setOpen(false)}
+            >
+              <FontAwesome name="window-close" size={24} color="#ff0000" />
+            </TouchableOpacity>
+            <Image
+              style={{ width: "90%", height: 300 }}
+              source={{ uri: capturedPhoto }}
+            />
+          </View>
+        </Modal>
+      )}
     </View>
   );
 }
@@ -13,8 +128,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  camera: {
+    width: 400,
+    height: 700,
+  },
+  button: { padding: 12 },
+  picture: {
+    width: 350,
+    height: 45,
+    borderRadius: 8,
+    backgroundColor: "#222",
+    marginTop: 10,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
